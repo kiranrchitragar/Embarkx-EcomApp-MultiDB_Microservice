26a1382c0e26e1ee5ced36f686fdd00554463c85
Kiran Chitragar <kiranphdt@gmail.com>
2025-10-09 03:25:11 +0530

    ZipKin Integration with Grafana

--- order-microservice/src/main/java/com/ecommerce/order/restClient/UserServiceClientConfig.java
+++ order-microservice/src/main/java/com/ecommerce/order/restClient/UserServiceClientConfig.java
@@ -1,10 +1,16 @@
 package com.ecommerce.order.restClient;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 
 import java.util.Optional;
@@ -13,13 +19,7 @@ import java.util.Optional;
 public class UserServiceClientConfig {
 
     @Bean
-    @LoadBalanced
-    public RestClient.Builder loadUserRestClientBuilder(){
-        return RestClient.builder();
-    }
-
-    @Bean
-    public RestClient userRestClientInterface(@Qualifier("loadUserRestClientBuilder") RestClient.Builder builder){
+    public RestClient userRestClientInterface(RestClient.Builder builder){
         return builder.baseUrl("http://user-microservice")
                 .defaultStatusHandler(HttpStatusCode::is4xxClientError,((request,response)-> Optional.empty()))
                 .build();
